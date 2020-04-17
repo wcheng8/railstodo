60afd03acd2ea878b5dcb330993e54284b37c130
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-04-17 23:00:42 +1000

    added a new todoitems inside todolist created a form within todolist created deleting

---
+++ app/controllers/todoitems_controller.rb
@@ -0,0 +1,24 @@
+class TodoitemsController < ApplicationController
+  before_action :set_todo_list
+  def create
+   @todoitem = @todolist.todoitems.create(todo_item_params)
+   redirect_to @todolist
+  end
+  def destroy
+    @todoitem = @todolist.todoitems.find(params[:id])
+    if @todoitem.destroy
+      flash[:success] = "Todo List item was deleted."
+    else
+      flash[:error] = "Todo List item could not be deleted."
+    end
+    redirect_to @todolist
+  end
+  private
+  def set_todo_list
+   @todolist = Todolist.find(params[:todolist_id])
+  end
+  def todo_item_params
+   params[:todoitem].permit(:content)
+  end
+
+end
